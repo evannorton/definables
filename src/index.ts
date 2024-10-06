5dfd828fab778c0b611d6ca3d292a26bed17e0b1
Evan Norton <evan@retro-mmo.com>
2024-10-06 03:36:35 -0500

    Add getDefinablesCount

--- src/index.ts
+++ src/index.ts
@@ -152,3 +152,7 @@ export const definableExists = <T extends Definable>(
   prototype: new (...args: any[]) => T,
   id: string,
 ): boolean => getDefinables(prototype).has(id);
+export const getDefinablesCount = <T extends Definable>(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- The args are not relevant to this function.
+  prototype: new (...args: any[]) => T,
+): number => getDefinables(prototype).size;
