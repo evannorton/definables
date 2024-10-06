2c755b25e9d99c7b32e6ddf0a6837832cb3ff9ce
Evan Norton <evan@retro-mmo.com>
2024-10-06 02:07:19 -0500

    Add definableExists

--- src/index.ts
+++ src/index.ts
@@ -146,3 +146,8 @@ export const getDefinable = <T extends Definable>(
   }
   return definable;
 };
+export const definableExists = <T extends Definable>(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any -- The args are not relevant to this function.
+  prototype: new (...args: any[]) => T,
+  id: string,
+): boolean => getDefinables(prototype).has(id);
